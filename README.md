4a14ff0259634841b646e390aabf6adb907c716a
bkimcode <bkimcode@users.noreply.github.com>
2023-02-02 23:06:47 +0000

    published by GitHub Actions

---
+++ README.md
@@ -0,0 +1,8 @@
+# Morea Template
+
+This repository contains a template Morea course site.
+
+For documentation on how to use Morea, please see https://morea-framework.github.io.
+
+EDIT: 
+to view current: https://spring2023moreaframeworkplugin.github.io/SampleMoreaWebsite/index.html
---
+++ schedule/schedule-info.js
@@ -0,0 +1,6 @@
+moreaEventData = [
+  {title: "E03: Host MySQL", url: "/SampleMoreaWebsite/morea/Relational-Databases-Create/experience-mysql-hostserver.html", start: "2023-01-21"},
+  {title: "Relational Databases Create", url: "/SampleMoreaWebsite/modules/relational-databases-create", start: "2023-01-13", end: "2023-01-31T23:00"},
+  {title: "E02: Download MySQL", url: "/SampleMoreaWebsite/morea/introduction/experience-download-mysql.html", start: "2023-01-15"},
+  {title: "ICS 321 Introduction", url: "/SampleMoreaWebsite/modules/introduction", start: "2023-01-13", end: "2023-01-31T23:00"}
+]
